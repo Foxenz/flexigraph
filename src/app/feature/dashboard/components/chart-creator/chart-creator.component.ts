4654b4fa95b842316e54ba5018ba73370dbd9fb1
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-07-05 10:04:00 +0200

    Structuration du projet

---
+++ src/app/feature/dashboard/components/chart-creator/chart-creator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-creator',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-creator.component.html',
+  styleUrl: './chart-creator.component.scss',
+})
+export class ChartCreatorComponent {}
---
+++ src/app/feature/dashboard/components/chart-zone/chart-zone.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-zone',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-zone.component.html',
+  styleUrl: './chart-zone.component.scss',
+})
+export class ChartZoneComponent {}
