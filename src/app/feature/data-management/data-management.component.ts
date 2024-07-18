43cfd84a32b56400dc058314a70b7cb231746dcc
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-07-18 16:56:49 +0200

    Affichage sous format de table avec bouton d'édit et de création

--- src/app/feature/data-management/data-management.component.ts
+++ src/app/feature/data-management/data-management.component.ts
@@ -1,15 +1,83 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { ChartManager } from '../../shared/managers/chart.manager';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from '@angular/material/table';
+import { JsonPipe, NgForOf } from '@angular/common';
+import { MatOption } from '@angular/material/autocomplete';
+import { MatFormField, MatLabel, MatSelect } from '@angular/material/select';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-data-management',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [
+    NavbarComponent,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatRowDef,
+    MatHeaderRowDef,
+    MatRow,
+    NgForOf,
+    MatOption,
+    MatSelect,
+    MatLabel,
+    MatFormField,
+    JsonPipe,
+    MatButton,
+    MatIcon,
+  ],
   templateUrl: './data-management.component.html',
-  styleUrl: './data-management.component.scss',
+  styleUrls: ['./data-management.component.scss'],
   providers: [ChartManager],
 })
 export class DataManagementComponent {
-  constructor(public chartManager: ChartManager) {}
+  displayedColumns: string[] = ['label', 'data', 'star'];
+  dataSource: { label: string; valuePerMonth: number[] }[] = [];
+  availableYears: number[] = [];
+  selectedYear: number;
+
+  constructor(public chartManager: ChartManager) {
+    this.initializeAvailableYears();
+    this.selectedYear = this.availableYears[0]; // Default to the first available year
+    this.initializeDataSource();
+  }
+
+  initializeAvailableYears() {
+    this.availableYears = this.chartManager.listOfData.map(data => data.year);
+  }
+
+  initializeDataSource() {
+    const dataForYear = this.chartManager.listOfData.find(
+      data => data.year === this.selectedYear
+    );
+    if (dataForYear) {
+      this.dataSource = dataForYear.data;
+    }
+  }
+
+  onYearChange(year: number) {
+    this.selectedYear = year;
+    this.initializeDataSource();
+  }
+
+  createData() {
+    console.log('Create data');
+  }
 }
