84e14bba49d7935cd4e07f3d3c40d5e193b74635
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-07-18 15:59:16 +0200

    Affichage des data de list of data

--- src/app/feature/data-management/data-management.component.ts
+++ src/app/feature/data-management/data-management.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { ChartManager } from '../../shared/managers/chart.manager';
 
 @Component({
   selector: 'app-data-management',
@@ -7,5 +8,8 @@ import { NavbarComponent } from '../../shared/components/navbar/navbar.component
   imports: [NavbarComponent],
   templateUrl: './data-management.component.html',
   styleUrl: './data-management.component.scss',
+  providers: [ChartManager],
 })
-export class DataManagementComponent {}
+export class DataManagementComponent {
+  constructor(public chartManager: ChartManager) {}
+}
