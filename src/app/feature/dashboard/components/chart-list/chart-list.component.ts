93c82154495bba556c45cfafbaf0c6921c7b0853
Alexandre <102592431+Foxenz@users.noreply.github.com>
2024-08-02 13:27:20 +0200

    Nouvelle affichage button et list

--- src/app/feature/dashboard/components/chart-list/chart-list.component.ts
+++ src/app/feature/dashboard/components/chart-list/chart-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { ChartItemComponent } from './chart-item/chart-item.component';
 import { ChartCardModel } from '../../models/chart-model';
-import { MatFabButton } from '@angular/material/button';
+import { MatButton, MatFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { filter } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
@@ -11,7 +11,7 @@ import { DialogFormChartComponent } from '../dialogs/dialog-chart/dialog-form-ch
 @Component({
   selector: 'app-chart-list',
   standalone: true,
-  imports: [ChartItemComponent, MatFabButton, MatIcon],
+  imports: [ChartItemComponent, MatFabButton, MatIcon, MatButton],
   templateUrl: './chart-list.component.html',
   styleUrl: './chart-list.component.scss',
 })
@@ -19,19 +19,6 @@ export class ChartListComponent {
   readonly dialog = inject(MatDialog);
 
   constructor(public chartManager: ChartManager) {}
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogFormChartComponent);
-
-    dialogRef
-      .afterClosed()
-      .pipe(filter(element => element != undefined))
-      .subscribe(result => this.createChart(result));
-  }
-
-  createChart(chart: ChartCardModel) {
-    this.chartManager.createChart(chart);
-  }
 
   changeVisibility(chart: ChartCardModel): void {
     this.chartManager.switchVisibility(chart.id);
--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnDestroy } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,6 +10,10 @@ import { ChartListComponent } from './components/chart-list/chart-list.component
 import { ChartManager } from '../../shared/managers/chart.manager';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { DialogFormChartComponent } from './components/dialogs/dialog-chart/dialog-form-chart.component';
+import { filter } from 'rxjs';
+import { ChartCardModel } from './models/chart-model';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dashboard',
@@ -33,8 +37,13 @@ export class DashboardComponent implements OnDestroy {
   mobileQuery: MediaQueryList;
 
   private readonly _mobileQueryListener: () => void;
+  readonly dialog = inject(MatDialog);
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    public chartManager: ChartManager,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
@@ -42,5 +51,18 @@ export class DashboardComponent implements OnDestroy {
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogFormChartComponent);
+
+    dialogRef
+      .afterClosed()
+      .pipe(filter(element => element != undefined))
+      .subscribe(result => this.createChart(result));
+  }
+
+  createChart(chart: ChartCardModel) {
+    this.chartManager.createChart(chart);
   }
 }
